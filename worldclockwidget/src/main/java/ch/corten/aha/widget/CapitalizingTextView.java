e11d806fe81d78d352ace7f24056774dc72d9603
Tom Saleeba <tom.saleeba@gmail.com>
2021-07-28 12:33:14 -0600

    chore: fix linting errors and gradle warnings

--- worldclockwidget/src/main/java/ch/corten/aha/widget/CapitalizingTextView.java
+++ worldclockwidget/src/main/java/ch/corten/aha/widget/CapitalizingTextView.java
@@ -23,9 +23,9 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Build;
 import android.util.AttributeSet;
-import android.widget.TextView;
+import androidx.appcompat.widget.AppCompatTextView;
 
-public class CapitalizingTextView extends TextView {
+public class CapitalizingTextView extends AppCompatTextView {
     private static final boolean SANS_ICE_CREAM = Build.VERSION.SDK_INT < Build.VERSION_CODES.ICE_CREAM_SANDWICH;
     private static final boolean IS_GINGERBREAD = Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD;
 
